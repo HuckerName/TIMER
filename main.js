eb54c479431bc0af8d04acf0fe58d6caed1283d0
HuckerName <122457184+HuckerName@users.noreply.github.com>
2023-03-03 11:47:22 +0300

    Add files via upload
    Timer

---
+++ main.js
@@ -0,0 +1,49 @@
+const HOURS = document.querySelector('.hours')
+const MINUTS = document.querySelector('.minuts')
+const SECOND = document.querySelector('.secunds')
+const BUTTONSTART = document.querySelector('.buttonStart')
+const BUTTONPAUSE = document.querySelector('.buttonPause')
+const BUTTONRESET = document.querySelector('.buttonReset')
+
+let hours = 0
+let minuts = 0
+let seconds = 0
+let active = false
+let once
+
+const startTimer = () => {
+  once = setInterval(() => {
+    HOURS.textContent = hours.toString().padStart(2, '0')
+    MINUTS.textContent = minuts.toString().padStart(2, '0')
+    SECOND.textContent = seconds.toString().padStart(2, '0')
+
+    seconds += 1
+    if (seconds === 60) {
+      seconds = 0
+      minuts += 1
+    }
+
+    if (minuts === 60) {
+      minuts = 0
+      hours += 1
+    }
+  }, 1000)
+}
+
+const stopTimer = () => {
+  clearInterval(once)
+}
+
+const resetTimer = () => {
+  HOURS.textContent = '00'
+  MINUTS.textContent = '00'
+  SECOND.textContent = '00'
+  hours = 0
+  minuts = 0
+  seconds = 0
+  clearInterval(once)
+}
+
+BUTTONSTART.addEventListener('click', startTimer)
+BUTTONRESET.addEventListener('click', resetTimer)
+BUTTONPAUSE.addEventListener('click', stopTimer)
